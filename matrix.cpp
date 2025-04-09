6d390d1a7760ca10caafa18105e994f55e855038
joeyipps04 <j546i501@ku.edu>
2025-04-08 22:50:12 -0500

    Copying all functions from previous lab
    Will test and see if these work for this class

--- matrix.cpp
+++ matrix.cpp
@@ -20,7 +20,7 @@ private:
     vector<vector<T>> data;
 
 public:
-matrix(int n) : size(n), data(n, vector<T>(n)) {}
+matrix(int N) : size(N), data(N, vector<T>(N)) {}
 
     void set(int i, int j, T value) {
         data[i][j] = value;
@@ -37,10 +37,54 @@ matrix(int n) : size(n), data(n, vector<T>(n)) {}
     void print() const {
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
-                cout << setw(5) << data[i][j] << " ";
+                cout << setw(8) << data[i][j] << " ";
             }
             cout << endl;
         }
     }
 
-    
+    Matrix add(const Matrix& other) const { //function to add two matrices
+        Matrix added(size);    //creates empty matrix of size N to store results
+        for (int i = 0; i < N; ++i) {   //iterates through rows
+            for (int j = 0; j < N; ++j) {   //iterates through columns
+                added.T[i][j] = T[i][j] + other.T[i][j];   //adds values of two matrices
+            }
+        }
+        return added;   //returns added matrix
+    }
+
+    Matrix multiply(const Matrix &other) const {    //function to multiply two matrices
+        Matrix multiplied(N);   //creates empty matrix of size N to store results
+        for (int i = 0; i < N; ++i) {   //iterates through rows
+            for (int j = 0; j < N; ++j) {   //iterates through columns
+                for (int k = 0; k < N; ++k) {   //iterates through columns of first matrix and rows of second matrix
+                    multiplied.T[i][j] += T[i][k] * other.T[k][j]; //multiplies values of two matrices
+                }
+            }
+        }
+        return multiplied;  //returns multiplied matrix
+    }
+
+    int diagonalsum() const {   //function add values of diagonal of matrix
+        int sum = 0;    //initializes sum to 0
+        for (int i = 0; i < N; ++i) {   //iterates through rows
+            sum += T[i][i];  //adds values of diagonal from top left to bottom right
+            sum += T[i][N-1-i];  //adds values of diagonal from top right to bottom left
+        }
+        return sum;  //returns sum of diagonals
+    }
+
+    void swaprows(int row1, int row2) { //function to swap two rows of matrix
+        swap(T[row1], T[row2]);   //swaps rows using std swap function
+    }
+    //used chatgpt to help find swap function 
+    void swapcolumns(int col1, int col2) {  //function to swap two columns of matrix
+        for (int i = 0; i < N; ++i) {   //iterates through rows
+            swap(T[i][col1], T[i][col2]); //swaps columns using std swap function
+        }   //have to iterate through rows to swap columns, by swapping each value individually
+    }
+
+    void chngevalue(int row, int col, int value) {  //function to change value of matrix at given row and column
+        T[row][col] = value; //sets value at given row and column to new value
+    }
+};
