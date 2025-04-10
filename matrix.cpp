5da2290aac0f888363ee6a708ea4df8740331a88
joeyipps04 <j546i501@ku.edu>
2025-04-09 20:05:30 -0500

    rewritten functions
    using correct variable names now. Also properly overloading adding and multiplying instead of functions like I had previously copied from the previous lab

--- matrix.cpp
+++ matrix.cpp
@@ -13,3 +13,49 @@
 
 using namespace std;
 
+template <typename T>
+class Matrix {
+private:
+    int N;  
+    vector<vector<T>> mtrx;  
+public:
+    Matrix(int size): N(size), mtrx(size, vector<T>(N)) {} 
+
+    void print() const {
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                cout << setw(8) << mtrx[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+    Matrix operator+(const Matrix &other) const {
+        Matrix result(N);
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                result.mtrx[i][j] = mtrx[i][j] + other.mtrx[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix operator*(const Matrix &other) const {
+        Matrix result(N);
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                for (int k = 0; k < N; ++k) {
+                    result.mtrx[i][j] += mtrx[i][k] * other.mtrx[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    T diagonalSum() const {
+        T sum = 0;
+        for (int i = 0; i < N; ++i) {
+            sum+= mtrx[i][i];  
+            sum += mtrx[i][N - 1 - i]; 
+        }
+    }
